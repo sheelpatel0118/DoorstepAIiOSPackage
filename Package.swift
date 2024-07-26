d0599339d40096ad88d6c1f94c023d9934f95a34
Sheel Patel <sheel@doorstep.ai>
2024-07-26 19:17:28 -0400

    test

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "DoorstepAIiOSSDK",
+            name: "DoorstepAIiOSPackage",
             targets: ["DoorstepAIiOSSDK"]
         ),
     ],
