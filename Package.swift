43e6b8dbc993e671b825e7c2190864aa999b578f
Sheel Patel <sheel@doorstep.ai>
2024-07-26 19:05:50 -0400

    test

--- Package.swift
+++ Package.swift
@@ -1,30 +1,34 @@
 // swift-tools-version: 5.10
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
-    name: "DoorstepAIiOSPackage",
+    name: "DoorstepAIiOSSDK",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14) // Specify the minimum iOS version your framework supports
     ],
     products: [
         .library(
-            name: "DoorstepAIiOSPackage",
-            targets: ["DoorstepAIiOSDependencies", "DoorstepAIiOSSDK"]),
+            name: "DoorstepAIiOSSDK",
+            targets: ["DoorstepAIiOSSDK"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/google-ar/arcore-ios-sdk", from: "1.40.0"),
-        .package(url: "https://github.com/ikhvorost/ReactBridge.git", from: "1.0.0")
+        // Add any other dependencies your framework needs
+        .package(url: "https://github.com/google-ar/arcore-ios-sdk", from: "1.0.0"),
     ],
     targets: [
-        .target(name: "DoorstepAIiOSDependencies", dependencies: [
-            "DoorstepAIiOSSDK",
-            .product(name: "ARCoreGeospatial", package: "arcore-ios-sdk"),
-            .product(name: "ARCoreGARSession", package: "arcore-ios-sdk")
-        ], path: "Sources/DoorstepAIiOSPackage"),
-        
         .binaryTarget(
             name: "DoorstepAIiOSSDK",
-            path: "./Sources/DoorstepAIiOSSDK.xcframework"
+            path: "./DoorstepAIiOSSDK.xcframework"
+        ),
+        .target(
+            name: "DoorstepAIiOSSDKWrapper",
+            dependencies: [
+                "DoorstepAIiOSSDK",
+                .product(name: "ARCoreGeospatial", package: "arcore-ios-sdk")
+            ],
+            path: "./Sources"
         )
     ]
 )
