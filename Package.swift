47c421f3a98530e0957a0fd13808710ed8dcb4a1
Sheel Patel <sheel@doorstep.ai>
2024-07-26 19:08:09 -0400

    test

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "DoorstepAIiOSSDK",
+    name: "DoorstepAIiOSSDKPackage",
     platforms: [
         .iOS(.v14) // Specify the minimum iOS version your framework supports
     ],
